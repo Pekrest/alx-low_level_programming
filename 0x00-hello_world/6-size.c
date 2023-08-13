cf1e67943ee2818d2e507c320e52e28dac7e9b4a
pekrest <peteroku3@gmail.com>
2023-08-13 09:08:08 -0700

    sizes on the computer

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Prints the size of various types on the computer
+ *
+ * Return: 0 (success)
+ *
+ */
+int main(void)
+{
+	char i;
+	int j;
+	long int k;
+	long long int l;
+	float m;
+
+	printf("Size of a char:%lu byte(s)\n", sizeof(i));
+	printf("Size of a int:%lu byte(s)\n", sizeof(j));
+	printf("Size of a long int:%lu byte(s)\n", sizeof(k));
+	printf("Size of a long long int:%lu byte(s)\n", sizeof(l));
+	printf("Size of a float:%lu byte(s)\n", sizeof(m));
+	return  (0);
+}
