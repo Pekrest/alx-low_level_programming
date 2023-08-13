3f9121ee5628ed60f6a731ce42f357bbc3800298
pekrest <peteroku3@gmail.com>
2023-08-13 09:50:55 -0700

    size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,5 +18,5 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", sizeof(k));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(l));
 	printf("Size of a float: %lu byte(s)\n", sizeof(m));
-	return  (0);
+	return (0);
 }
