75cb913220b235fa7f0d33bee33750b37b77d996
pekrest <peteroku3@gmail.com>
2023-08-17 13:49:07 -0700

    agent Jack Bauer time is ticking

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * jack_bauer - a function that prints every minute of the day of Jack Bauer,
+ * starting from 00:00 to 23:59
+ *
+ */
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i <= 2; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (k = 0; k <= 5; k++)
+			{
+				for (l = 0; l <= 9; l++)
+				{
+					if (i >= 2 && j >= 4)
+						break;
+				_putchar(i + 48);
+				_putchar(j + 48);
+				_putchar(58);
+				_putchar(k + 48);
+				_putchar(j + 48);
+				_putchar('\n');
+				}
+			}
+		}
+	}
+}
