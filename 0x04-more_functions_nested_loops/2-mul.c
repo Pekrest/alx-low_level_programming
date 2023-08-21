0780de15b2aad01298fb0c67c3cf886708d8636d
pekrest <peteroku3@gmail.com>
2023-08-21 14:36:02 -0700

    Muls law

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * mul - a function that multiples 2 int
+ *
+ * @a: first int
+ * @b: second int
+ *
+ * Return: 0 (success)
+ *
+ */
+int mul(int a, int b)
+{
+	int mx;
+
+	mx = a * b;
+	return (mx);
+}
