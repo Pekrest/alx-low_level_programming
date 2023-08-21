edcbe98b5a0ad0facdfaab3337b383356eda0766
pekrest <peteroku3@gmail.com>
2023-08-21 15:37:59 -0700

    Numb speak for yourself

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - a function that prints the numbers,
+ * from 0 to 9, followed by \n
+ *
+ * Return: 0 (success)
+ *
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
