0e04bc3576fb3fa03d23f4235cd2df30125952be
pekrest <peteroku3@gmail.com>
2023-08-21 11:00:43 -0700

    104 Fibon

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,25 +8,36 @@
  */
 int main(void)
 {
-	unsigned long int f1 = 1;
-	unsigned long int f2 = 2;
-	unsigned long int next_f;
+	unsigned long int n;
+	unsigned long int a = 1;
+	unsigned long int i = 2;
+	unsigned long int m = 1000000000;
+	unsigned long int a1;
+	unsigned long int a2;
+	unsigned long int i1;
+	unsigned long int i2;
 
-	
-	unsigned long int i;
-	
-	printf("%lu, %lu, ", f1, f2);
-	for (int i = 3; i <= 98; i++)
+	printf("%lu", a);
+	for (n = 1; n < 91; n++)
 	{
-		next_f = f1 + f2;
-		printf("%lu", next_f);
-	if (i != 98)
-	{
-		printf(", ");
+		printf(", %lu", i);
+		i += a;
+		a = i - a;
 	}
-	f1 = f2;
-	f2 = next_f;
+	a1 = (a / m);
+	a2 = (a % m);
+	i1 = (i / m);
+	i2 = (i % m);
+	for (n = 92; n < 99; ++n)
+	{
+		printf(", %lu", i1 + (i2 / m));
+		printf("%lu", i2 % m);
+		i1 = i1 + a1;
+		a1 = i1 - a1;
+		i2 = i2 + a2;
+		a2 = i2 - a2;
 	}
 	printf("\n");
 	return (0);
 }
+
