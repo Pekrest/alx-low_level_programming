f832af22236c88070d2171644faf54fdbdc65e8d
pekrest <peteroku3@gmail.com>
2023-08-20 18:54:49 -0700

    summing power

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * add - a function that adds two integers and returns the result.
+ *
+ * @a: integer to check
+ * @b: integer to check
+ *
+ * Return: s (a && b sum)
+ *
+ */
+int add(int a, int b)
+{
+	int s;
+
+	s = a + b;
+	return (s);
+}
