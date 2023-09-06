0c32abcb444405578a8c41c78ea9b54950c2f896
pekrest <peteroku3@gmail.com>
2023-09-06 10:50:15 -0700

    multiply those two nummbeerss 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - a program that multiplies two numbers.
+ *
+ * @argc: int input (argument count)
+ * @argv: array of char ( argument vector)
+ *
+ * Return: 0 (success)
+ *
+ */
+int main(int argc, char *argv[])
+{
+	int x, mul = 1;
+
+	if (argc != 3)
+	{
+		printf("error\n");
+		return (1);
+	}
+	for (x = 1; x < argc; x++)
+	{
+		mul *= atoi(argv[x]);
+	}
+	printf("%d\n", mul);
+	return (0);
+}
