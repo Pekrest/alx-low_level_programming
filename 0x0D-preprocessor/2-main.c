671c0b2c9d1be4fa161eb919afaf12a957bacbe8
pekrest <peteroku3@gmail.com>
2023-09-12 19:43:17 -0700

    sum it all upp

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- * main -  a program that prints the name of the file it was compiled from, followed by \n
+ * main -  a program that prints the name of the file it was compiled from,
+ * followed by \n
  *
  * Return: 0 (success)
  */
