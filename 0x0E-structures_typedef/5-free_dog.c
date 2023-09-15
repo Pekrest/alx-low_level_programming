31ec23023a04106b222fab4aff05df6ca7c57595
pekrest <peteroku3@gmail.com>
2023-09-14 18:57:13 -0700

    many legs on tail, what am i?

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - a function that frees dogs.
+ * @d: struct dog
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
