b517ae57d23a155cace7ef0873e846a7a4eca7fd
pekrest <peteroku3@gmail.com>
2023-11-06 01:11:41 -0800

    all my argss

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - a program that prints its name, followed by a new line.
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	argc = argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
