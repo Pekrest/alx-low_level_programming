fdfa716fc01237036233468b7d367f7bd1e23375
pekrest <peteroku3@gmail.com>
2023-11-06 01:29:38 -0800

    silence arguments are the worst

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - a program that prints the number of arguments passed into it.
+ *
+ * @argc: argument counts
+ * @argv: argument vector
+ *
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	argv = argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
+
